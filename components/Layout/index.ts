292d4e3b8722ac6c370af88c43729bdef7f2a0e0
Miguel Rivas <rivasmig@gmail.com>
2026-09-16 11:40:16 +0200

    Create index.ts to export Header and Footer components

---
+++ components/Layout/index.ts
@@ -0,0 +1,13 @@
+export { Header } from './Header/Header'
+export { Footer } from './Footer/Footer'
+
+export type {
+  HeaderProps,
+  NavigationItem,
+} from './Header/Header'
+
+export type {
+  FooterProps,
+  FooterColumn,
+  FooterLink,
+} from './Footer/Footer'

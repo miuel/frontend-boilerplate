64842007b7579a93f5b9d7f67ed445b97cb7156c
Miguel Rivas <rivasmig@gmail.com>
2026-09-16 17:15:39 +0200

    Export Drawer components and types from index.ts

---
+++ components/Drawer/index.ts
@@ -0,0 +1,11 @@
+export {
+  Drawer,
+  type DrawerProps,
+} from './Drawer'
+
+export {
+  DrawerContent,
+  DrawerHeader,
+  type DrawerContentProps,
+  type DrawerHeaderProps,
+} from './DrawerContent'

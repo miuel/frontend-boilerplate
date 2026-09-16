a8a475aecfd9bec9ae1d6d36b2409d4fdab1f06c
Miguel Rivas <rivasmig@gmail.com>
2026-09-16 17:00:04 +0200

    Export form components and their types from index.ts

---
+++ components/Form/index.ts
@@ -0,0 +1,23 @@
+export {
+  Input,
+  Textarea,
+  type InputProps,
+  type TextareaProps,
+} from './Input'
+
+export {
+  Checkbox,
+  type CheckboxProps,
+} from './Checkbox'
+
+export {
+  Radio,
+  type RadioProps,
+} from './Radio'
+
+export {
+  Select,
+  SelectItem,
+  type SelectProps,
+  type SelectItemProps,
+} from './Select'

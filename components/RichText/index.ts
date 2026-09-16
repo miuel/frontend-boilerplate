f37ee97c45bacff44fa5b8123abd4fe02389afbd
Miguel Rivas <rivasmig@gmail.com>
2026-09-16 10:34:05 +0200

    Export components and types from RichText module

---
+++ components/RichText/index.ts
@@ -0,0 +1,11 @@
+export { RichText } from './RichText'
+export { ContentTransformer } from './ContentTransformer'
+
+export type { RichTextProps } from './RichText'
+export type {
+  NodeProps,
+  NodeMetadata,
+  Override,
+  Overrides,
+  ContentTransformerProps,
+} from './ContentTransformer'

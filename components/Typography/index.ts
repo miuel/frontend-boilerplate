afea03ec6f988d23e10e01c0364a40c29e958b64
Miguel Rivas <rivasmig@gmail.com>
2026-09-16 10:16:41 +0200

    Create index.ts

---
+++ components/Typography/index.ts
@@ -0,0 +1,13 @@
+export { Heading } from './Heading'
+export { Subheading } from './Subheading'
+export { Text } from './Text'
+
+export type { HeadingProps, HeadingSize } from './Heading'
+export type { SubheadingProps, SubheadingSize } from './Subheading'
+export type { TextProps, TextSize } from './Text'
+
+export type {
+  Breakpoint,
+  ResponsiveSize,
+  TypographyBaseProps,
+} from './utils'

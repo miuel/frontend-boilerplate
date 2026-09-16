fd47a28f2c96cf252250d764f69a3dbaeb03a445
Miguel Rivas <rivasmig@gmail.com>
2026-09-16 10:50:31 +0200

    Create index.ts

---
+++ components/Icon/index.ts
@@ -0,0 +1,13 @@
+export {
+  IconArrowRight,
+  IconPlus,
+  IconMinus,
+  IconChevronDown,
+  IconChevronUp,
+  IconClose,
+  IconSearch,
+  IconMenu,
+  IconLoader,
+} from './Icon'
+
+export type { IconProps } from './Icon'

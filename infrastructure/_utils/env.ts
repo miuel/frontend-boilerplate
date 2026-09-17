bbaf911bb0f4ee12b8fe6602863e3306f25a3436
Miguel Rivas <rivasmig@gmail.com>
2026-09-17 12:49:57 +0200

    Delete infrastructure directory

--- infrastructure/_utils/env.ts
+++
@@ -1,9 +0,0 @@
-export function getEnvVariables(keys: string[]) {
-  const values = keys.map(key => {
-    const value = process.env[key]
-    if (!value) throw new Error(`Missing ENV variable: ${key}`)
-    return value
-  })
-
-  return values
-}
